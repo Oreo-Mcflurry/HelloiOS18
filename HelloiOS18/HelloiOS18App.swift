9d6d27393642a41ff1f0d96d49281b926fbe0616
유인호 <yuiop1029@naver.com>
2024-06-11 20:30:06 +0900

    Initial Commit

---
+++ HelloiOS18/HelloiOS18App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloiOS18App.swift
+//  HelloiOS18
+//
+//  Created by A_Mcflurry on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloiOS18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloiOS18Tests/HelloiOS18Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloiOS18Tests.swift
+//  HelloiOS18Tests
+//
+//  Created by A_Mcflurry on 6/11/24.
+//
+
+import Testing
+@testable import HelloiOS18
+
+struct HelloiOS18Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
